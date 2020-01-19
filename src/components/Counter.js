4da63429394827acf9223cb27b5f8e7c0513d291
Chris Kalfas <christopherkalfas22@gmail.com>
2020-01-19 15:37:04 -0500

    refactor Counter component to stateless and lifted state from that component to app-js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,29 +1,24 @@
-import React, { Component }from 'react'
+import React from 'react'
 
-class Counter extends Component {
-    state = { 
-        score: 0 
-    };
-    
-    incrementScore = () => {
-        this.setState( prevState => ({
-            score: prevState.score + 1
-        }));
-    }
-    decrementScore = () => {
-        this.setState( prevState => ({
-            score: prevState.score - 1
-    }));
-    }
-    render() {
+const Counter = props => {
+   
+    // incrementScore = () => {
+    //     this.setState( prevState => ({
+    //         score: prevState.score + 1
+    //     }));
+    // }
+    // decrementScore = () => {
+    //     this.setState( prevState => ({
+    //         score: prevState.score - 1
+    // }));
+    // }
         return (
         <div className="counter">
-            <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
-            <span className="counter-score">{ this.state.score }</span>
-            <button className="counter-action increment" onClick={this.incrementScore}> + </button>
+            <button className="counter-action decrement" > - </button>
+            <span className="counter-score">{ props.score }</span>
+            <button className="counter-action increment" > + </button>
         </div>
         );
-    }
 }
 
 export default Counter
